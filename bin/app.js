a9960453982bc23aacbe7768e11b7437583cc179
RuslanR <rusjlan28@gmail.com>
2021-10-22 16:52:31 +0300

    add options and arguments

--- bin/app.js
+++ bin/app.js
@@ -4,6 +4,8 @@ import { Command } from 'commander/esm.mjs';
 const program = new Command();
 
 program
-  .version('1.0.0')
-  .description('Compares two configuration files and shows a difference.');
+  .version('0.0.2')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
 program.parse(process.argv);
